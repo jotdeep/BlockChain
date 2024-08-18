46375588759dde84faa435ecb7f958bf77d3cf7d
Jotdeep Singh <jsahluwalia11@gmail.com>
2024-08-18 16:41:49 +0530

    Added code for Genesis Block

--- blockchain/block.go
+++ blockchain/block.go
@@ -17,5 +17,12 @@ func (b* Block) ComputeHash(){
 	computedHash :=md5.Sum(concatenatedData)
 
 	b.Hash = string(computedHash[:])
-
+}
+func CreateBlock(data string, prevHash string) *Block {
+    block := &Block{"", data, prevHash}
+    block.ComputeHash()
+    return block
+}
+func Genesis() *Block {
+    return CreateBlock("Genesis", "")
 }
